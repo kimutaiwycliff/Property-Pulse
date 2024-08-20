c47cdd72b3c1a2766791f577a3a1458e44253ee6
Wycliff Kimutai <kcliff@tutanota.com>
2024-08-20 16:32:54 +0300

    feat(navbar_component):setting up state and layout

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,6 @@
+const PropertyPage = ({params}) => {
+  return (
+    <div>Property Page {params.id}</div>
+  )
+}
+export default PropertyPage
---
+++ app/properties/page.jsx
@@ -0,0 +1,6 @@
+const ProportiesPage = () => {
+  return (
+    <div>ProportiesPage</div>
+  )
+}
+export default ProportiesPage
