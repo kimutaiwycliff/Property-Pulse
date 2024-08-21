afccfb05e74bcd822eeb9da65319d2e800c9fd0f
Wycliff Kimutai <kcliff@tutanota.com>
2024-08-21 10:09:19 +0300

    home properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
   return <>
   <Hero />
   <InfoBoxes />
+  <HomeProperties />
   </>;
 };
 
