94a7e61beb0563f3d49bae8dd2522110c948710f
Wycliff Kimutai <kcliff@tutanota.com>
2024-08-21 09:55:16 +0300

    feat(profileCard component):map over properties json

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,20 @@
-const ProportiesPage = () => {
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
+const PropertiesPage = () => {
   return (
-    <div>ProportiesPage</div>
-  )
-}
-export default ProportiesPage
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-46">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default PropertiesPage;
