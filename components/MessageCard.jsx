d1f31b150903306e57525c69b40b2c60f442b48a
Wycliff Kimutai <kcliff@tutanota.com>
2024-08-26 14:57:57 +0300

    notification through global context state

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -4,18 +4,22 @@ import { toast } from "react-toastify";
 import markMessageAsRead from "@/app/actions/markMessagesAsRead";
 import { set } from "mongoose";
 import deleteMessage from "@/app/actions/deleteMessage";
+import { useGlobal } from "@/context/GlobalContext";
 
 const MessageCard = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
   const [isDeleted, setIsDeleted] = useState(false);
+  const {setUnreadCount} = useGlobal();
   const handleDeleteClick = async () => {
     await deleteMessage(message._id);
     setIsDeleted(true);
+    setUnreadCount((prev) => (read ? prev - 1 : prev + 1));
     toast.success("Message deleted");
   };
   const handleReadCLick = async () => {
     const read = await markMessageAsRead(message._id);
     setIsRead(read);
+    setUnreadCount((prev) => (read ? prev - 1 : prev + 1));
     toast.success(`Message marked as ${read ? "Read" : "New"}`);
   };
   if (isDeleted) {
--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -1,10 +1,21 @@
 'use client';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import getUnreadMessageCount from '@/app/actions/getUnreadMessageCount';
+import { get } from 'mongoose';
 
 const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
     const [unreadCount, setUnreadCount] = useState(0);
+    const {data: session} = useSession();
+    useEffect(() => {
+        if (session && session.user) {
+            getUnreadMessageCount().then((res) => {
+                if (res.count) setUnreadCount(res.count);
+            })
+        }
+    }, [getUnreadMessageCount, session]);
 
     return (
         <GlobalContext.Provider value={{ unreadCount, setUnreadCount }}>
